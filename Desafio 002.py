50d6051cd6ac9b1724ecddb731d886d29e3e80e1
DenisPython1987 <denisandervivan2@gmail.com>
2026-05-03 20:02:21 -0300

    Initial commit

---
+++ Desafio 002.py
@@ -0,0 +1,5 @@
+dia_nasc = input("Em qual dia você nasceu? ")
+mes_nasc = input("Em qual mês você nasceu? ")
+ano_nasc = input("Em qual ano você nasceu? ")
+
+print("Você nasceu no dia " + dia_nasc + " e no mês de " + mes_nasc + " do ano de " + ano_nasc)
---
+++ Desafio 003.py
@@ -0,0 +1,5 @@
+num_1 = int(input("Digite o primeiro número: "))
+num_2 = int(input("Digite o segundo número: "))
+
+print(num_1 + num_2)
+            
