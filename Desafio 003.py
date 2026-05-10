c814a758935a419e055e0473556c783d5a30ecd9
DenisPython1987 <denisandervivan2@gmail.com>
2026-05-10 19:41:55 -0300

    Exercíses

--- Desafio 003.py
+++ Desafio 003.py
@@ -1,5 +1,4 @@
 num_1 = int(input("Digite o primeiro número: "))
 num_2 = int(input("Digite o segundo número: "))
 
-print(num_1 + num_2)
-            
+print("A soma entre ", num_1, " e ", num_2, " vale ", num_1 + num_2)
---
+++ Desafio 004.py
@@ -0,0 +1,6 @@
+algo = input('Digite algo: ')
+
+print(f'Tipo primitivo= {type(algo)}')
+print(f'É numérico = {algo.isnumeric()}')
+print(f'É letra = {algo.isalpha()}')
+print(f'É alfanumérico = {algo.isalnum()}')
