bbb35d0eff3b208b4a65d7608b6b4e10ca6a3ff9
DenisPython1987 <denisandervivan2@gmail.com>
2026-07-02 09:48:06 -0300

    Exercíses

--- Desafio_083.py
+++ Desafio_083.py
@@ -1,21 +1,15 @@
-esquerda = direita = contador = 0
+# Esse daqui eu desisti de fazer porque eu não dei conta
+
+esquerda = direita = 0
 
 frase = str(input("Digite uma expressão com parênteses: ")).strip().upper()
-while contador < len(frase):
-    frase = str(input("Digite uma expressão com parênteses: ")).strip().upper()
-    if "()" in frase:
-        contador += 1
-        for caractere in frase:
-            if caractere == "(":
-                esquerda += 1
-                for parêntese in frase:
-                    if parêntese == ")":
-                        direita += 1
-                        break
-
-    else:
-        print("Digite uma frase com parênteses! Tente novamente.")
-        continue
+for posição, caractere in enumerate(frase):
+    if caractere == "(":
+        esquerda += 1
+        for parêntese in frase[posição:]:
+            if parêntese == ")":
+                direita += 1
+                break
 
 if esquerda == direita:
     print("A frase digitada é uma experessão válida")
