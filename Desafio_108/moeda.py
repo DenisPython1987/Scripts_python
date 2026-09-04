1f5abb2360f2d18959c687f47f458a398c47a72e
Denisander Vivan <105618584+DenisPython1987@users.noreply.github.com>
2026-09-04 16:46:53 -0300

    Atualizar o moeda.py

--- Desafio_108/moeda.py
+++ Desafio_108/moeda.py
@@ -1,14 +1,26 @@
-def aumentar(valor, porcentagem):
+def aumentar(valor: float, porcentagem: float) -> float:
+    """Função para aumentar um valor com base num percentual. Retorna um float.
+    Parâmetro valor: float que será aumentado.
+    Parâmetro porcentagem: float que será o percentual de aumento."""
     return valor + (valor * porcentagem / 100)
 
-def diminuir(valor, porcentagem):
+def diminuir(valor: float, porcentagem: float) -> float:
+    """Função para diminuir um valor com base em um percentual. Retorna um float.
+    Parâmetro valor: float que será diminuído.
+    Parâmetro porcentagem: float que será o percentual de decremento."""
     return valor - (valor * porcentagem / 100)
 
-def dobro(valor):
+def dobro(valor: float):
+    """Função que dobra um valor. Retorna um float.
+    Parâmetro valor: float que será dobrado."""
     return valor * 2
 
-def metade(valor):
+def metade(valor: float) -> float:
+    """Função que diminui um valor pela metade. Retorna um float.
+    Parâmetro valor: float que será diminuído."""
     return valor / 2
 
-def moeda(valor):
+def moeda(valor: float) -> str:
+    """Função que retorna um valor float no formato de moeda brasileira. Retorna uma string.
+    Parâmetro valor: float que será processado."""
     return f'R${valor:.2f}'.replace('.', ',')
