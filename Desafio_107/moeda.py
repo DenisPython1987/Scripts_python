f8097c01b19fe02b3f9b9217e3ce08a19b50d20d
Denisander Vivan <105618584+DenisPython1987@users.noreply.github.com>
2026-09-04 16:03:33 -0300

    Atualizar o moeda.py

--- Desafio_107/moeda.py
+++ Desafio_107/moeda.py
@@ -1,21 +1,21 @@
-def aumentar(valor: int, porcentagem: float) -> float:
+def aumentar(valor: float, porcentagem: float) -> float:
     """Função para aumentar um valor com base em um percentual. Retorna um float.
-    Parâmetro valor: int ou float que será aumentado.
+    Parâmetro valor: float que será aumentado.
     Parâmetro porcentagem: float que representa o percentual de aumento"""
     return valor + (valor * porcentagem / 100)
 
-def diminuir(valor: int, porcentagem: float) -> float:
+def diminuir(valor: float, porcentagem: float) -> float:
     """Função para diminuir um valor com base num percentual. Retorna um float.
-    Parâmetro valor: inteiro ou float que será diminuído.
+    Parâmetro valor: float que será diminuído.
     Parâmetro percentual: float que representa o percentual a ser diminuído."""
     return valor - (valor * porcentagem / 100)
 
-def dobro(valor: int) -> int:
+def dobro(valor: float) -> float:
     """Função para dobrar um valor. Pode retornar um int ou um float, conforme a entrada.
-    Parâmetro valor: int ou float que será dobrado."""
+    Parâmetro valor: float que será dobrado."""
     return valor * 2
 
-def metade(valor: int) -> float:
+def metade(valor: float) -> float:
     """Função para dividir um número pela metade. Retorna sempre um float.
-    Parâmetro valor: int ou float que será dividido."""
+    Parâmetro valor: float que será dividido."""
     return valor / 2
